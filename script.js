015ff8b9bde038ad36a3333d9231ddf9084ab701
4nndmw <anandamaulanaw1850@gmail.com>
2023-08-04 00:15:53 +0700

    update

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+// SKILLS
+
+$(".skill-per").each(function () {
+  var $this = $(this);
+  var per = $this.attr("per");
+  $this.css("width", per + "%");
+  $({ animatedValue: 0 }).animate(
+    { animatedValue: per },
+    {
+      duration: 1000,
+      step: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+      complete: function () {
+        $this.attr("per", Math.floor(this.animatedValue) + "%");
+      },
+    }
+  );
+});
+
+// END SKILLS
